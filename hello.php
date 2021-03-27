66ba4c1bded36164f3d3c2b5a5a0793cfe72fe47
Leroy Rosales <leroyrosales@gmail.com>
2021-03-27 07:35:20 -0500

    Fix typo

--- hello.php
+++ hello.php
@@ -5,7 +5,7 @@
  */
 /*
 Plugin Name: Hello WAP
-Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in one word sung most famously by Carid B and Megan Thee Stallion: WAP. When activated you will randomly see a lyric from <cite>WAP</cite> in the upper right of your admin screen on every page.
+Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in one word sung most famously by Cardi B and Megan Thee Stallion: WAP. When activated you will randomly see a lyric from <cite>WAP</cite> in the upper right of your admin screen on every page.
 Author: Leroy Rosales
 Version: 1.0
 Author URI: https://leroyrosales.com
